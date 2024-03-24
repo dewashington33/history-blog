cae16464191b8ec2b5cf614c0df8854a5fa6504e
dewashington33 <dewashington@hotmail.com>
2024-03-24 00:11:54 -0400

    Initial commit

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const SITE_TITLE = "Perry Area Historical Society";
+export const SITE_DESCRIPTION =
+  "Interesting stories, history, and facts about Perry, Georgia and the surrounding area.";
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
+export const NAVBAR_TITLE_1 = "Perry Area";
+export const NAVBAR_TITLE_2 = "Historical Society";
+export const ABOUT_DESCRIPTION =
+  "Learn more about the Perry Area Historical Society and its mission.";
